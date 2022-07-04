7b6abc0c59a45221b80318837afaeada14947eee
Victor Nogueira <felladrin@gmail.com>
2022-07-05 01:11:25 +0300

    Initial commit

---
+++ install.sh
@@ -0,0 +1,25 @@
+if [ -x "$(command -v brew)" ]; then
+  brew tap \
+    helix-editor/helix
+
+  brew install \
+    zellij \
+    micro \
+    helix \
+    gitui \
+    procs \
+    tmux \
+    exa \
+    bat \
+    lf
+fi
+
+echo "Installing Ble.sh (https://github.com/akinomyoga/ble.sh)" &&
+  curl -L https://github.com/akinomyoga/ble.sh/releases/download/nightly/ble-nightly.tar.xz | tar xJf - &&
+  mv ble-nightly* blesh &&
+  mkdir -p ~/.local/share &&
+  mv blesh/ ~/.local/share &&
+  echo 'source ~/.local/share/blesh/ble.sh' >> ~/.bashrc
+
+echo "Adding bash aliases" &&
+  (ALIAS='lf -last-dir-path=$HOME/.lf-last-dir-path; LASTDIR=`cat $HOME/.lf-last-dir-path`; cd "$LASTDIR";'; echo "alias lf='$ALIAS';") >> ~/.bash_aliases
