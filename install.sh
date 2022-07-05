2964078a1506da8102d163668e50c6d57fe4feec
Victor Nogueira <felladrin@gmail.com>
2022-07-05 21:47:21 +0000

    Add config for the History time format

--- install.sh
+++ install.sh
@@ -22,6 +22,10 @@ echo "Installing Ble.sh (https://github.com/akinomyoga/ble.sh)..." &&
   echo 'source ~/.local/share/blesh/ble.sh' >> ~/.bashrc &&
   echo "Done!"
 
+echo "Configuring bash..." &&
+  echo 'HISTTIMEFORMAT="|%Y-%m-%d %T| "' >> ~/.bashrc &&
+  echo "Done!"
+
 echo "Adding bash aliases..." &&
   (ALIAS='lf -last-dir-path=$HOME/.lf-last-dir-path; LASTDIR=`cat $HOME/.lf-last-dir-path`; cd "$LASTDIR";'; echo "alias lf='$ALIAS';") >> ~/.bash_aliases &&
   echo "alias cloudcmd='npx -y cloudcmd';" >> ~/.bash_aliases &&
