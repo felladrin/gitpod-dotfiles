08cf085443cb7a182bcbef01ce3eaec68f29bfdc
Victor Nogueira <felladrin@gmail.com>
2022-07-06 19:03:58 +0300

    Print the install process duration at the end

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+startTime=$(date +%s)
+
 if [ -x "$(command -v brew)" ]; then
   brew tap \
     helix-editor/helix
@@ -30,3 +32,7 @@ echo "Adding bash aliases..." &&
   (ALIAS='lf -last-dir-path=$HOME/.lf-last-dir-path; LASTDIR=`cat $HOME/.lf-last-dir-path`; cd "$LASTDIR";'; echo "alias lf='$ALIAS';") >> ~/.bash_aliases &&
   echo "alias cloudcmd='npx -y cloudcmd';" >> ~/.bash_aliases &&
   echo "Done!"
+
+endTime=$(date +%s)
+
+echo "Install finished in $((endTime-startTime)) seconds."
