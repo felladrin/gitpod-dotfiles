e57f3d7afe8cfca46d57844699db902053105737
Victor Nogueira <felladrin@gmail.com>
2022-07-13 20:28:24 +0300

    Install SpaceVim in place of Micro Text Editor

--- install.sh
+++ install.sh
@@ -5,36 +5,37 @@ if [ -x "$(command -v brew)" ]; then
     lazygit \
     ripgrep \
     zellij \
-    micro \
     procs \
     bat \
     fzf \
     lf
 fi
 
-if [ -x "$(command -v micro)" ]; then
-  micro -plugin install \
-    lsp \
-    fzf
+if [ -x "$(command -v vim)" ]; then
+  echo "Installing SpaceVim..." &&
+    curl -sLf https://spacevim.org/install.sh | bash &&
+    echo "Done!"
 fi
 
-echo "Installing Ble.sh (https://github.com/akinomyoga/ble.sh)..." &&
-  curl -L https://github.com/akinomyoga/ble.sh/releases/download/nightly/ble-nightly.tar.xz | tar xJf - &&
-  mv ble-nightly* blesh &&
-  mkdir -p ~/.local/share &&
-  mv blesh/ ~/.local/share &&
-  echo 'source ~/.local/share/blesh/ble.sh' >> ~/.bashrc &&
-  echo "Done!"
+if [ -x "$(command -v bash)" ]; then
+  echo "Configuring bash..." &&
+    echo 'HISTTIMEFORMAT="|%Y-%m-%d %T| "' >> ~/.bashrc &&
+    echo 'FZF_DEFAULT_COMMAND="rg --files"' >> ~/.bashrc &&
+    echo "Done!"
 
-echo "Configuring bash..." &&
-  echo 'HISTTIMEFORMAT="|%Y-%m-%d %T| "' >> ~/.bashrc &&
-  echo 'FZF_DEFAULT_COMMAND="rg --files"' >> ~/.bashrc &&
-  echo "Done!"
+  echo "Adding bash aliases..." &&
+    (ALIAS='lf -last-dir-path=$HOME/.lf-last-dir-path; LASTDIR=`cat $HOME/.lf-last-dir-path`; cd "$LASTDIR";'; echo "alias lf='$ALIAS';") >> ~/.bash_aliases &&
+    echo "alias cloudcmd='npx -y cloudcmd';" >> ~/.bash_aliases &&
+    echo "Done!"
 
-echo "Adding bash aliases..." &&
-  (ALIAS='lf -last-dir-path=$HOME/.lf-last-dir-path; LASTDIR=`cat $HOME/.lf-last-dir-path`; cd "$LASTDIR";'; echo "alias lf='$ALIAS';") >> ~/.bash_aliases &&
-  echo "alias cloudcmd='npx -y cloudcmd';" >> ~/.bash_aliases &&
-  echo "Done!"
+  echo "Installing Ble.sh (https://github.com/akinomyoga/ble.sh)..." &&
+    curl -L https://github.com/akinomyoga/ble.sh/releases/download/nightly/ble-nightly.tar.xz | tar xJf - &&
+    mv ble-nightly* blesh &&
+    mkdir -p ~/.local/share &&
+    mv blesh/ ~/.local/share &&
+    echo 'source ~/.local/share/blesh/ble.sh' >> ~/.bashrc &&
+    echo "Done!"
+fi
 
 endTime=$(date +%s)
 
